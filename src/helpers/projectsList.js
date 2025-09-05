7ca9613a9f715b27d4da273c00900409412e1ccf
cheradam13 <133335424+cheradam13@users.noreply.github.com>
2025-09-05 16:45:19 +0700

    changed the site

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,59 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01.jpg";
+
+import project02 from "./../img/projects/02.jpg";
+import project02Big from "./../img/projects/02.jpg";
+
+import project03 from "./../img/projects/03.jpg";
+import project03Big from "./../img/projects/03.jpg";
+
+import project04 from "./../img/projects/04.jpg";
+import project04Big from "./../img/projects/04.jpg";
+
+import project05 from "./../img/projects/05.jpg";
+import project05Big from "./../img/projects/05.jpg";
+
+const projects = [
+	{
+		title: 'Lagoona',
+		skills: 'HTML, CSS',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: 'https://github.com/cheradam13/Lagoona',
+		liveSiteLink: "https://cheradam13.github.io/Lagoona/",
+	},
+	{
+		title: 'VK Маруся',
+		skills: 'ReactJS, Typescript, Redux',
+		img: project02,
+		imgBig: project02Big,
+		gitHubLink: 'https://github.com/cheradam13/VK-Marusya',
+		liveSiteLink: "https://cheradam13.github.io/VK-Marusya/",
+	},
+	{
+		title: 'Realtime Weather App',
+		skills: 'JavaScript, API',
+		img: project03,
+		imgBig: project03Big,
+		gitHubLink: 'https://github.com/cheradam13/Realtime-Weather-App',
+		liveSiteLink: "https://cheradam13.github.io/Realtime-Weather-App/",
+	},
+	{
+		title: 'CRM',
+		skills: 'JavaScript',
+		img: project04,
+		imgBig: project04Big,
+		gitHubLink: 'https://github.com/cheradam13/CRM',
+		liveSiteLink: "https://cheradam13.github.io/CRM/",
+	},
+	{
+		title: 'Blanchard',
+		skills: 'HTML, CSS, Figma',
+		img: project05,
+		imgBig: project05Big,
+		gitHubLink: 'https://github.com/cheradam13/Blanchard',
+		liveSiteLink: "https://cheradam13.github.io/Blanchard/",
+	},
+];
+
+export {projects}
---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Barnaul, Russia</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram / WhatsApp</h2>
+						<p>
+							<a href="tel:+79132421225">+7 (913) 242-12-25</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:adam13pad@yandex.ru">
+								adam13pad@yandex.ru
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
